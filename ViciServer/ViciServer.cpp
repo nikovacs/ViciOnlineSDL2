ab39c57956fce007c3f18eb172e339d4aa705d66
Nikolas Kovacs <kovacsn00@gmail.com>
2023-06-07 14:16:27 -0400

    server loop

--- ViciServer/ViciServer.cpp
+++ ViciServer/ViciServer.cpp
@@ -1,6 +1,8 @@
 #include "ViciServer.h"
+#include <chrono>
+#include <thread>
 
-ViciServer::ViciServer() : _started{ false } {
+ViciServer::ViciServer() : _running{ false } {
 	
 }
 
@@ -9,5 +11,13 @@ ViciServer::~ViciServer() {
 }
 
 void ViciServer::start() {
-	_started = true;
+	_running = true;
+	serverLoop();
+}
+
+void ViciServer::serverLoop() {
+	while (_running) {
+		// do stuff
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000 / TICKS_PER_SECOND));
+	}
 }
--- ViciServer/ViciServer.h
+++ ViciServer/ViciServer.h
@@ -1,9 +1,11 @@
 #pragma once
 class ViciServer {
 private:
-	bool _started;
+	static const int TICKS_PER_SECOND{ 20 };
+	bool _running;
 public:
 	ViciServer();
 	virtual ~ViciServer();
 	void start();
+	void serverLoop();
 };
