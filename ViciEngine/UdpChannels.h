9f41a68a447b7c665a0a28b182e1226a1543daf0
Nikolas Kovacs <kovacsn00@gmail.com>
2023-06-12 14:43:20 -0400

    Finish TypeChannelMap. Get channelID from type

--- ViciEngine/UdpChannels.h
+++ ViciEngine/UdpChannels.h
@@ -16,5 +16,11 @@ namespace Networking {
 		static inline const std::map<std::type_index, int> _typeToNumberMap{
 			{ std::type_index(typeid(AssetTypes::Texture)), Texture},
 		};
+	public:
+		template <typename T>
+		static int getChannel() {
+			std::type_index typeIndex(typeid(T));
+			return _typeToNumberMap.at(typeIndex);
+		}
 	};
 }
