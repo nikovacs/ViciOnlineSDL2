6b145c4cd46e1ee22101562ba4315bd9147a0204
Nikolas Kovacs <kovacsn00@gmail.com>
2023-06-12 14:40:37 -0400

    No longer needed.

--- ViciClient/TextureManager.cpp
+++
@@ -1,6 +0,0 @@
-#include "TextureManager.h"
-#include "ViciClient.h"
-
-SDL_Texture* Textures::loadTexture(const char* fileName) {
-	return IMG_LoadTexture(ViciClient::instance->getRenderer(), fileName);
-}
--- ViciClient/TextureManager.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-
-namespace Textures {
-	SDL_Texture* loadTexture(const char* fileName);
-}
