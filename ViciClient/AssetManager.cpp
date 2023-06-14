27d4e6f6d62ad7b1df04a20e9fb98e0209daa8f0
Nikolas Kovacs <kovacsn00@gmail.com>
2023-06-14 13:55:34 -0400

    add hack fix for messages failing to send.

--- ViciClient/AssetManager.cpp
+++ ViciClient/AssetManager.cpp
@@ -21,6 +21,7 @@ std::unordered_map<std::string, std::shared_ptr<void>> Networking::AssetManager:
 
 
 void Networking::AssetManager::requestFile(std::string_view fileName, int channelID) {
+	std::cout << "requesting file\n";
 	const uint8_t* buffer = reinterpret_cast<const uint8_t*>(fileName.data());
 	size_t bufferSize = fileName.length();
 
@@ -28,7 +29,12 @@ void Networking::AssetManager::requestFile(std::string_view fileName, int channe
 	ENetPacket* packet = enet_packet_create(buffer, bufferSize, ENET_PACKET_FLAG_RELIABLE);
 
 	// Send the packet to the destination peer on the desired channel
-	enet_peer_send(static_cast<Networking::UdpClient*>(Networking::UdpClient::instance)->getGameServer(), channelID, packet);
+	int ret; 
+	// TODO: FIX BAD IMPLEMENTATION. AFTER SO MANY FAILED ATTEMPTS, THE CLIENT SHOULD TIME OUT AND DISCONNECT.
+	// TODO: IT IS POSSIBLE THAT THIS IS DUE TO VISUAL STUDIO STARTING THE SERVER ONLY IMMEDIATELY BEFORE THE CLIENT AND THE SERVER DOES NOT HAVE ENOUGH TIME TO START
+	do {
+		ret = enet_peer_send(static_cast<Networking::UdpClient*>(Networking::UdpClient::instance)->getGameServer(), channelID, packet);
+	} while (ret < 0);
 	
 	// Flush the client
 	enet_host_flush(Networking::UdpClient::instance->getHost());
