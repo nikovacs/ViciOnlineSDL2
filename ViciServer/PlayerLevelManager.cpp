152cac9e8cc67be9545e68446821b752c06b8493
Nikolas Kovacs <kovacsn00@gmail.com>
2023-08-12 10:36:56 -0400

    PlayerLevelManager init commit

---
+++ ViciServer/PlayerLevelManager.cpp
@@ -0,0 +1,31 @@
+#include "PlayerLevelManager.hpp"
+
+namespace Networking {
+	std::unordered_map<std::string, std::set<uint32_t>> _playersOnLevel = {};
+	std::unordered_map<std::string, std::set<uint32_t>> _playersWatchingLevel = {};
+	std::mutex _lvlMngrMtx = {};
+	
+	void PlayerLevelManager::addToLevel(uint32_t id, std::string_view levelName) {
+		if (!_playersOnLevel.contains(levelName.data())) {
+			_playersOnLevel.emplace(levelName, std::set<uint32_t>{});
+		}
+		_playersOnLevel.at(levelName.data()).insert(id);
+	}
+
+	void PlayerLevelManager::removeFromLevel(uint32_t id, std::string_view levelName) {
+		if (!_playersOnLevel.contains(levelName.data())) return; // consider removing empty set?
+		_playersOnLevel.at(levelName.data()).erase(id);
+	}
+
+	void PlayerLevelManager::startWatchingLevel(uint32_t id, std::string_view levelName) {
+		if (!_playersWatchingLevel.contains(levelName.data())) {
+			_playersWatchingLevel.emplace(levelName, std::set<uint32_t>{});
+		}
+		_playersWatchingLevel.at(levelName.data()).insert(id);
+	}
+
+	void PlayerLevelManager::stopWatchingLevel(uint32_t id, std::string_view levelName) {
+		if (!_playersWatchingLevel.contains(levelName.data())) return; // consider removing empty set?
+		_playersWatchingLevel.at(levelName.data()).erase(id);
+	}
+}
