7567d5ea7e4cb77b8dec04c51cb400852cf37a19
Matt Kosack <matthew.kosack@gmail.com>
2023-10-05 20:34:16 -0400

    change the cosmetic view for login

--- ViciClient/LoginScene.cpp
+++ ViciClient/LoginScene.cpp
@@ -1,6 +1,6 @@
 #include "LoginScene.hpp"
 
-#include <iostream>
+#include <iostream> 
 
 namespace Scenes {
     LoginButtonListener::LoginButtonListener(std::function<void()>& onLoginCallback) : _callbackOnProcess{ onLoginCallback } {}
@@ -17,37 +17,61 @@ namespace Scenes {
 					<style>
 						body {
                             font-family: "Applestorm";
+                            /* TODO: I don't have the knowledge of how a scene works to solve this right now, but only hald the page is considered body?*/
+                            /* My assumption is only half of the screen is rendered through rmlui, the other half is something else*/
+                            background: gray;
                         }
 
+                        .title-box,
                         .login-box {
+                            display: flex;
                             width: 300px;
                             padding: 20px;
-                            border: 1px solid #ccc;
                             border-radius: 5px;
                             text-align: center;
-                            display: flex;
                             justify-content: center;
                             align-items: center;
-                            margin: 0;
+                            margin: auto;
                         }
 
+                        .title-box {
+                            font-effect: glow( 2px #ee9 );
+                            font-size: 48px;
+                        }
+                        
                         input[type="text"],
                         input[type="password"],
                         button {
                             width: 100%;
                             padding: 10px;
                             margin-bottom: 10px;
-                            border: 1px solid #ccc;
                             border-radius: 3px;
+                            border: 1px solid #CCC000; /* warning presents with this line, but removal deletes the outlines*/
+                            font-size: 24px;
+                        }
+
+                        label {
+                            font-size: 24px;
                         }
 					</style>
                 </head>
                 <body>
-                    <div class="login-box">
+                    <div class="title-box">
                         <h2>User Login</h2>
+                    </div>
+
+                    <div class="login-box">
                         <form>
-                            <input type="text" placeholder="Username" required></input><br></br>
-                            <input type="password" placeholder="Password" required></input><br></br>
+                            <div class="input-container">
+                                <label for="username">Username</label>
+                                <input type="text" id="username" required></input>
+                            </div>
+                            <div class="input-container">
+                                <label for="password">Password</label>
+                                <input type="password" required></input>
+                            </div>
+                            <br></br>
+                            <br></br>
                             <button id="loginButton" type="submit" onclick="">Login</button>
                         </form>
                     </div>
