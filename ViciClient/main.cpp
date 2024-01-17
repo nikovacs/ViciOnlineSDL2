14d2fd5b83fd504a6c5ff6a42dca9e7617fb7abb
Nikolas Kovacs <kovacsn00@gmail.com>
2024-01-16 21:11:24 -0500

    make client match refresh rate of monitor

--- ViciClient/main.cpp
+++ ViciClient/main.cpp
@@ -5,19 +5,21 @@
 #include <iostream>
 
 int main() {
-	constexpr int FPS{ 60 };
-	constexpr int frameDelay{ 1000 / FPS };
+	ViciClient client{ "Vici Online", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false };
+	client.start();
+	client.initialize();
+
+	SDL_DisplayMode displayMode;
+	SDL_GetCurrentDisplayMode(0, &displayMode);
+	std::cout << "Display refresh rate: " << displayMode.refresh_rate << std::endl;
+
+	int FPS{ displayMode.refresh_rate };
+	int frameDelay{ 1000 / FPS };
 	Uint32 frameStart{ 0 };
 	Uint32 frameEnd{ 0 };
 	int frameTime{ 0 };
 	TimeManager::setDelta(0);
 	
-	
-	ViciClient client = ViciClient("Vici Online", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
-	client.start();
-
-	client.initialize();
-
 	while (client.isRunning()) {
 		Uint32 previousFrameStart{ frameStart };
 		frameStart = SDL_GetTicks();
