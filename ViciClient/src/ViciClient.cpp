91156f5db0549f50699759696a41cf6dbf422d77
Nikolas Kovacs <kovacsn00@gmail.com>
2024-09-16 22:06:24 -0400

    correctly poll sdl events

--- ViciClient/src/ViciClient.cpp
+++ ViciClient/src/ViciClient.cpp
@@ -1,96 +1,99 @@
+#include "ViciClient/include/ViciClient.hpp"
+#include "ViciClient/include/Handlers/KeyboardInputHandler.hpp"
+#include "ViciClient/include/Handlers/MouseInputHandler.hpp"
+#include "ViciClient/include/Networking/AssetManager.hpp"
+#include "ViciClient/include/Networking/UdpClient.hpp"
+#include "ViciClient/include/Scenes/Scene.hpp"
 #include <SDL2/SDL.h>
 #include <iostream>
-#include <thread>
-#include <playfab/PlayFabSettings.h>
 #include <playfab/PlayFabApiSettings.h>
-#include "../../ViciClient/include/ViciClient.hpp"
-#include "../../ViciClient/include/Scenes/Scene.hpp"
-#include "../../ViciClient/include/Networking/UdpClient.hpp"
-#include "../../ViciClient/include/Networking/AssetManager.hpp"
-#include "../../ViciClient/include/Handlers/KeyboardInputHandler.hpp"
-#include "../../ViciClient/include/Handlers/MouseInputHandler.hpp"
+#include <playfab/PlayFabSettings.h>
+#include <thread>
 
-ViciClient::ViciClient(const char* title, int xpos, int ypos, int width, int height, bool fullscreen)
-	: _isRunning{ false }, _window{ nullptr }, _renderer{ nullptr }, _sceneManager{ std::make_unique<Scenes::SceneManager>() } {
-	instance = this;
+ViciClient::ViciClient(const char *title, int xpos, int ypos, int width, int height, bool fullscreen)
+    : _isRunning{false}, _window{nullptr}, _renderer{nullptr}, _sceneManager{std::make_unique<Scenes::SceneManager>()} {
+    instance = this;
 
-	int flags{ 0 };
-	flags |= fullscreen ? SDL_WINDOW_FULLSCREEN : SDL_WINDOW_MAXIMIZED | SDL_WINDOW_RESIZABLE;
-	
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) { return; }
-	_window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
-	_renderer = SDL_CreateRenderer(_window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
-	SDL_SetRenderDrawColor(_renderer, 0, 0, 0, 255);
+    int flags{0};
+    flags |= fullscreen ? SDL_WINDOW_FULLSCREEN : SDL_WINDOW_MAXIMIZED | SDL_WINDOW_RESIZABLE;
 
-	Networking::AssetManager::generatePermanentAssets();
-	GUI::ViciGuiHost::initialize(_window, _renderer);
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        return;
+    }
+    _window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
+    _renderer = SDL_CreateRenderer(_window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
+    SDL_SetRenderDrawColor(_renderer, 0, 0, 0, 255);
 
-	_sceneManager->newLoginScene(
-		[this]()->void {
-			_sceneManager->newGameScene();
-		}
-	);
-	_sceneManager->setScene("LoginScene");
+    Networking::AssetManager::generatePermanentAssets();
+    GUI::ViciGuiHost::initialize(_window, _renderer);
+
+    _sceneManager->newLoginScene(
+        [this]() -> void {
+            _sceneManager->newGameScene();
+        });
+    _sceneManager->setScene("LoginScene");
 }
 
 ViciClient::~ViciClient() {
-	// scene manager needs to dispose of rmlContexts before ViciGuiHost deinitializes
-	_sceneManager.reset();
-	GUI::ViciGuiHost::deinitialize();
-	// Clearing the cache is required before deleting the renderer
-	// otherwise a debug assert may, or may not, be triggered
-	Networking::AssetManager::clearCache();
-	SDL_DestroyWindow(_window);
-	SDL_DestroyRenderer(_renderer);
-	SDL_Quit();
+    // scene manager needs to dispose of rmlContexts before ViciGuiHost deinitializes
+    _sceneManager.reset();
+    GUI::ViciGuiHost::deinitialize();
+    // Clearing the cache is required before deleting the renderer
+    // otherwise a debug assert may, or may not, be triggered
+    Networking::AssetManager::clearCache();
+    SDL_DestroyWindow(_window);
+    SDL_DestroyRenderer(_renderer);
+    SDL_Quit();
 }
 
 void ViciClient::handleEvents() {
-	SDL_Event event;
-	SDL_PollEvent(&event);
-	_sceneManager->handleEvents(&event);
+    SDL_Event event;
 
-	switch (event.type) {
-	case SDL_QUIT:
-		stop();
-		break;
-	case SDL_KEYDOWN:
-		Handlers::KeyboardInputHandler::handleKeyDown(event);
-		break;
-	case SDL_KEYUP:
-		Handlers::KeyboardInputHandler::handleKeyUp(event);
-		break;
-	case SDL_MOUSEBUTTONDOWN:
-		Handlers::MouseInputHandler::handleMouseDown(event);
-		break;
-	case SDL_MOUSEBUTTONUP:
-		Handlers::MouseInputHandler::handleMouseUp(event);
-		break;
-	default:
-		break;
-	}
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+        case SDL_QUIT:
+            stop();
+            break;
+        case SDL_KEYDOWN:
+            Handlers::KeyboardInputHandler::handleKeyDown(event);
+            break;
+        case SDL_KEYUP:
+            Handlers::KeyboardInputHandler::handleKeyUp(event);
+            break;
+        case SDL_MOUSEBUTTONDOWN:
+            Handlers::MouseInputHandler::handleMouseDown(event);
+            break;
+        case SDL_MOUSEBUTTONUP:
+            Handlers::MouseInputHandler::handleMouseUp(event);
+            break;
+        default:
+            break;
+        }
+
+        _sceneManager->handleEvents(&event);
+    }
 }
 
 void ViciClient::initialize() {
-	PlayFab::PlayFabSettings::staticSettings->titleId = "E76BA";
+    PlayFab::PlayFabSettings::staticSettings->titleId = "E76BA";
 }
 
 void ViciClient::update() {
-	_sceneManager->update();
+    _sceneManager->update();
 }
 
 void ViciClient::render() {
-	SDL_RenderClear(_renderer);
-	// do rendering here
-	_sceneManager->render(_renderer);
-	
-	SDL_RenderPresent(_renderer);
+    SDL_RenderClear(_renderer);
+    // do rendering here
+    _sceneManager->render(_renderer);
+
+    SDL_RenderPresent(_renderer);
 }
 
 void ViciClient::start() {
-	_isRunning = true;
+    _isRunning = true;
 }
 
 void ViciClient::stop() {
-	_isRunning = false;
+    _isRunning = false;
 }
